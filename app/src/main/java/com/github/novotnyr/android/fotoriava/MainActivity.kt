4681396ea94856740913d990db10bd4f5716847f
Robert Novotny <novotnr0@gmail.com>
2021-04-25 00:33:02 +0200

    Pridany widget fotoaparatu do kodu aktivity a cvaknutie fotky

--- app/src/main/java/com/github/novotnyr/android/fotoriava/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/android/fotoriava/MainActivity.kt
@@ -3,12 +3,20 @@ package com.github.novotnyr.android.fotoriava
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.otaliastudios.cameraview.CameraView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cameraView: CameraView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraView = findViewById(R.id.cameraView)
+        cameraView.setLifecycleOwner(this)
     }
 
-    fun onTakePhotoClick(view: View) {}
+    fun onTakePhotoClick(view: View) {
+        cameraView.takePicture()
+    }
 }
