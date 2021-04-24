0b227f3d2a6d4f74dd91e0beda6193581a34c99e
Robert Novotny <novotnr0@gmail.com>
2021-04-25 00:19:26 +0200

    Pridany hladacik a tlacidlo na fotenie

--- app/src/main/java/com/github/novotnyr/android/fotoriava/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/android/fotoriava/MainActivity.kt
@@ -2,10 +2,13 @@ package com.github.novotnyr.android.fotoriava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onTakePhotoClick(view: View) {}
 }
