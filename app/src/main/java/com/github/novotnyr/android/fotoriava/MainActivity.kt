9c87c3e88169cdb3dbcab2fe5866d576f6893f89
Robert Novotny <novotnr0@gmail.com>
2021-04-25 01:25:20 +0200

    Pridany nahladovy ImageView pre ostatnu fotku

--- app/src/main/java/com/github/novotnyr/android/fotoriava/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/android/fotoriava/MainActivity.kt
@@ -3,10 +3,13 @@ package com.github.novotnyr.android.fotoriava
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import com.otaliastudios.cameraview.CameraView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var cameraView: CameraView
+
+    private lateinit var lastPhotoImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
         cameraView = findViewById(R.id.cameraView)
         cameraView.setLifecycleOwner(this)
+
+        lastPhotoImageView = findViewById(R.id.lastPhotoImageView)
     }
 
     fun onTakePhotoClick(view: View) {
